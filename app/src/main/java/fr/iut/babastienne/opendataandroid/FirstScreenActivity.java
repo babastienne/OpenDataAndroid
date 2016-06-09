31bf65a59c2c179bdf37a5f19d1f17c2cd1f1788
babastienne <potiron.bastien@gmail.com>
2016-06-09 19:15:28 +0200

    Initial commit

---
+++ app/src/main/java/fr/iut/babastienne/opendataandroid/FirstScreenActivity.java
@@ -0,0 +1,13 @@
+package fr.iut.babastienne.opendataandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_screen);
+    }
+}
